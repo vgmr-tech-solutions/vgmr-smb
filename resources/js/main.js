929c5e21e3980f5603496dcc7004126b08c13367
sandeep-vgmr <66026655+sandeep-vgmr@users.noreply.github.com>
2020-08-25 12:38:51 +0530

    added files

---
+++ resources/js/main.js
@@ -0,0 +1,58 @@
+
+!(function($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+ 
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+var width = $(window). width(); 
+
+if(width < 991) {
+   $('header nav').click(function(){
+	//alert('ok');
+	$('body').toggleClass('menu-open');
+});
+       
+}
+
+
+
+
+})(jQuery);
+
